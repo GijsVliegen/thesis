961fc19a2b39c2b1eb17e97ea3e5aab0a895575f
r0810400 <r0810400@kuleuven.be>
2024-05-02 23:34:04 +0200

    gewerkt met vcs, job werkt nu goed, vcs docx ook beter nu

--- parallelTimerModule.py
+++ parallelTimerModule.py
@@ -2,14 +2,12 @@ from mpi4py import MPI
 import numpy as np
 from heuristicApplier import HeuristicApply, SddVarAppearancesList, SddVtreeCountList
 from heuristicApplier import RANDOM, IVO_LR, IVO_RL, \
-    KE, VP, VP_KE, VO, \
-    EL
+    KE, VP, VP_KE, VO, EL, VP_EL
 from heuristicApplier import AND, OR
 import timeit
+import os
+import sys
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
 
 def doHeuristicTest(heuristics, heuristicApplier, operation, overheadTime):
     timeHeuristics = []
@@ -45,19 +43,20 @@ def doHeuristicTest(heuristics, heuristicApplier, operation, overheadTime):
 #             for i in range(len(heuristics)):
 #                 file.write(f"heuristiek {heuristics[i]} times: {heuristicsTimes[i]}\n")
                 
-def heuristicApply(heuristics, operation, overheadTime):
-    nrOfSdds=20
-    iterationsPerNode = 5
-    nrOfVars=20
-    vtree = "balanced"
-    
-    if rank == 0:
-        data = [(i, i + iterationsPerNode) for i in range(1, iterationsPerNode*size, iterationsPerNode)]
-    else:
-        data = None
-    data = comm.scatter(data, root=0)
+def heuristicApply(nrOfVars, iterationsPerNode, vtree, overheadTime):
 
-    for i in range(int(nrOfVars/2), int(nrOfVars), int(nrOfVars*0.5)):
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    heuristics = [RANDOM, VO, IVO_LR, IVO_RL, VP_KE, VP_EL]
+    operation = OR
+    nrOfSdds = 20
+    # iterationsPerNode = 5
+    # nrOfVars=20
+    # vtree = "balanced"
+    
+
+    for i in range(int(nrOfVars/2), int(nrOfVars*5), int(nrOfVars*0.5)):
         nrOfClauses = i
         heuristicsTimes = np.zeros((len(heuristics), iterationsPerNode))
         heuristicApplier = HeuristicApply(nrOfSdds, nrOfVars, nrOfClauses, vtree_type=vtree)
@@ -73,9 +72,30 @@ def heuristicApply(heuristics, operation, overheadTime):
             allHeuristicTimes = np.concatenate(heuristicTimesMatrices, axis=1)
             name = "test" if overheadTime else "noOverhead"
             operationStr = "OR" if operation == OR else "AND"
-            with open(f"output/heuristic/{name}_{nrOfSdds}_{nrOfVars}_{nrOfClauses}_{operationStr}_{vtree}_{heuristics}.txt", 'w') as file:
+            data_directory = os.environ.get("VSC_DATA")
+            if data_directory is None:
+                data_directory = ""
+            local_file_path = f"output/heuristic/{name}_{nrOfSdds}_{nrOfVars}_{nrOfClauses}_{operationStr}_{vtree}_{heuristics}.txt"
+            fullPath = os.path.join(data_directory, local_file_path)
+            os.makedirs(os.path.dirname(fullPath), exist_ok=True)
+            with open(fullPath, 'w') as file:
                 file.write(f"experiment: sdds: {nrOfSdds}, vars: {nrOfVars}, operation = {operationStr}, vtree = {vtree}, heuristiek = {heuristics}" + '\n')
                 for i in range(len(heuristics)):
                     file.write(f"heuristiek {heuristics[i]} times: {allHeuristicTimes[i]}\n")
 
-heuristicApply([8, 7], OR, True)
+def main():
+    args = sys.argv[1:]
+    baseArgs = [10, 20, "balanced", True]
+    modifier = [lambda x: int(x), lambda x: int(x), lambda x: x, lambda x: x.lower() in ('true', '1', 't', 'y', 'yes')]
+    try:
+        for i in range(len(args)):
+            baseArgs[i] = modifier[i](args[i])
+    except ValueError:
+        print("Error: Please provide 4 valid arguments.")
+        return
+    # print(baseArgs)
+    heuristicApply(baseArgs[0], baseArgs[1], baseArgs[2], baseArgs[3])
+
+if __name__ == "__main__":
+    # Call main function with command line arguments excluding script name
+    main()
