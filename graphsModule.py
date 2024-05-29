cf43112c65618a49d46c0e5801d4d3dd777de531
r0810400 <r0810400@kuleuven.be>
2024-05-29 02:20:39 +0200

    graphing voor random ratios

--- graphsModule.py
+++ graphsModule.py
@@ -52,10 +52,7 @@ def getListFromLine(line):
         print(f"Invalid list format in line: {line}")
     return current_list
 
-def binplotter(ax1, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, indexCounter, colors, noOverhead = False):
-    filename = f"output/heuristic/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
-    if noOverhead:
-        filename = f"output/heuristic/noOverhead_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
+def binplotter(ax1, filename, heuristic, indexCounter, colors):
     lines = []
     try:
         with open(filename, 'r') as file:
@@ -73,10 +70,7 @@ def binplotter(ax1, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, index
             print(f"Invalid list format in line: {line}")
 
 
-def boxplotter(ax1, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, indexCounter, colors, noOverhead = False):
-    filename = f"output/heuristic/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
-    if noOverhead:
-        filename = f"output/heuristic/noOverhead_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
+def boxplotter(ax1, filename, indexCounter, colors):
     lines = []
     try:
         with open(filename, 'r') as file:
@@ -94,15 +88,8 @@ def boxplotter(ax1, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, index
             color = colors[indexCounter]
             for patch in box['boxes']:
                 patch.set_facecolor(color)
-            # for whisker in box['whiskers']: #verticale lijn
-            #     whisker.set_color('red') 
-            # for cap in box['caps']: #horizontale uiteinden
-            #     cap.set_color('blue')
             for median in box['medians']: #gemiddelde
                 median.set_color('black')
-            # for flier in box['fliers']: #uitschieters
-            #     flier.set_markerfacecolor('green')
-            #     flier.set_markeredgecolor('orange')
 
             ax1.axhline(y=np.mean(heuristicList), xmin=indexCounter/7+0.01, xmax=indexCounter/7 + 1/7 -0.01, color='black', linestyle='-')
         except SyntaxError:
@@ -117,28 +104,22 @@ def heuristicsPlot(heuristics, nrOfVars, vtree, operation):
 
         indexCounter = 0
         for heuristic in heuristics:
-            # {testName}_{nrOfSdds}_{nrOfVars}_{operationStr}_{vtree}_{heuristics}_{nrOfClauses}
-            boxplotter(ax1, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, indexCounter, colors)
+            filename = f"output/heuristic/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
+            boxplotter(ax1, filename, indexCounter, colors)
             # binplotter(ax1, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, indexCounter, colors)
             indexCounter += 1
-        boxplotter(ax1, nrOfVars, operationStr, vtree, VP_EL, nrOfClauses, indexCounter, colors, noOverhead = True)
+        filename = f"output/heuristic/noOverhead_20_{nrOfVars}_{operationStr}_{vtree}_{[VP_EL]}_{nrOfClauses}.txt"
+        boxplotter(ax1, filename, indexCounter, colors)
         # binplotter(ax1, nrOfVars, operationStr, vtree, VP_EL, nrOfClauses, indexCounter, colors, noOverhead = True)
         indexCounter += 1
-        
 
         ratio = nrOfClauses/nrOfVars
         titleOperation = "Disjunctie" if operation == OR else "Conjunctie"
         plt.title(f"{titleOperation}: compilatietijd voor r = {ratio}")
-        # plt.legend()
-
-
-
-
         plt.xticks(list(range(indexCounter)), list(map(getHeuristicName, heuristics)) + ["VP + EL 2"])
         plt.xlabel('heuristiek')
         plt.ylabel('tijd (s)')
         plt.yscale("log")
-
 
         ax2 = ax1.twinx()
         y_min, y_max = ax1.get_ylim()  # Get the limits from the left y-axis
@@ -147,26 +128,16 @@ def heuristicsPlot(heuristics, nrOfVars, vtree, operation):
         ax1.spines['right'].set_visible(False)
         ax2.spines['left'].set_visible(False)
         ax2.yaxis.tick_right()
-        
-        #binplots
-        # plt.xlabel('tijd (s)')
-        # plt.ylabel('aantal compilaties')
-        # plt.xscale('log')
-        
-
-        #boxplots
-        # ax1.set_yscale("log")
         ax2.set_yscale("log")
 
-        
         local_file_path = f"figs/heuristics/{vtree}/test_20_{nrOfVars}_{operationStr}_{heuristics+[10]}_{nrOfClauses}.png"
         fullPath = os.path.join("", local_file_path)
         os.makedirs(os.path.dirname(fullPath), exist_ok=True)
         plt.savefig(local_file_path)
         plt.clf() #clear
 
-def plotter(metric, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, indexCounter, colors):
-    filename = f"output/{metric}/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
+def plotter(filename, heuristic, indexCounter, colors):
+    # 
     lines = []
     try:
         with open(filename, 'r') as file:
@@ -181,8 +152,6 @@ def plotter(metric, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, index
             print(f"Invalid list format in line: {line}")
         plt.plot(range(1, 20), current_list, marker='o', \
                     linestyle='-', color = colors[indexCounter], label=getHeuristicName(heuristic))
-
-
 
 def otherMetricsPlot(heuristics, nrOfVars, vtree, operation):
     nrOfSdds=20
@@ -199,7 +168,8 @@ def otherMetricsPlot(heuristics, nrOfVars, vtree, operation):
         for nrOfClauses in range(int(nrOfVars*0.5), nrOfVars*5, int(nrOfVars*0.5)):
             indexCounter = 0
             for heuristic in heuristics:
-                plotter(metric, nrOfVars, operationStr, vtree, heuristic, nrOfClauses, indexCounter, colors)
+                filename = f"output/{metric}/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}_{nrOfClauses}.txt"
+                plotter(filename, heuristic, indexCounter, colors)
                 indexCounter += 1
 
             plt.xlabel('Index tussenresultaat')
@@ -215,16 +185,96 @@ def otherMetricsPlot(heuristics, nrOfVars, vtree, operation):
             plt.savefig(local_file_path) #savefig moet blijkbaar voor show() komen
             plt.clf()
 
+def randomRatiosPlot(heuristics, nrOfVars, vtree, operation):
+    nrOfSdds=20
+    operationStr = "OR" if operation == OR else "AND"
+    overhead = True#False#True
+    testName = "test" if overhead else "noOverhead"
+    colors = ['red', 'lightblue', "green", "orange", "purple", "brown", "yellow", "pink"]
+    #sizes
+    metrics = ["sizes", "varCounts", "depth"]
+    ylabels = ['Grootte van tussenresultaat', 'aantal variabelen in tussenresultaat', 'hoogte van tussenresultaat']
+    titles = ["groottes tussenresultaten voor r =", "aantal vars per tussenresultaat voor r =", "diepte tussenresultaten voor r ="]
+    yscale = ['log', 'linear', 'linear']
+
+    for (i, metric) in enumerate(metrics):
+        indexCounter = 0
+        for heuristic in heuristics:
+            filename = f"output/randomRatios/{metric}/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}.txt"
+            plotter(filename, heuristic, indexCounter, colors)
+            indexCounter += 1
+
+        plt.xlabel('Index tussenresultaat')
+        plt.ylabel(ylabels[i])
+        titleOperation = "Disjunctie" if operation == OR else "Conjunctie"
+        plt.title(titleOperation + ": " + titles[i])
+        plt.legend()
+        plt.xticks(range(1, 21, 2))
+        plt.yscale(yscale[i])
+        local_file_path = f"figs/randomRatios/{metric}/{vtree}/{testName}_{nrOfSdds}_{nrOfVars}_{operationStr}_{heuristics+[10]}.png"
+        fullPath = os.path.join("", local_file_path)
+        os.makedirs(os.path.dirname(fullPath), exist_ok=True)   
+        plt.savefig(local_file_path) #savefig moet blijkbaar voor show() komen
+        plt.clf()
+    
+    operationStr = "OR" if operation == OR else "AND"
+    colors = ['red', 'lightblue', "green", "orange", "purple", "brown", "pink", "yellow"]
+    fig, ax1 = plt.subplots()
+
+    indexCounter = 0
+    for heuristic in heuristics:
+        filename = f"output/randomRatios/heuristic/test_20_{nrOfVars}_{operationStr}_{vtree}_{[heuristic]}.txt"
+        # boxplotter(ax1, filename, indexCounter, colors)
+        binplotter(ax1, filename, heuristic, indexCounter, colors)
+        indexCounter += 1
+    filename = f"output/randomRatios/heuristic/noOverhead_20_{nrOfVars}_{operationStr}_{vtree}_{[VP_EL]}.txt"
+    # boxplotter(ax1, filename, indexCounter, colors)
+    binplotter(ax1, filename, heuristic, indexCounter, colors)
+    indexCounter += 1
+    
+    titleOperation = "Disjunctie" if operation == OR else "Conjunctie"
+    plt.title(f"{titleOperation}: compilatietijd")
+    
+    #binplots
+    plt.xscale("log")
+    plt.xlabel("tijd (s)")
+    plt.ylabel("aantal iteraties")
+    
+    #boxplots
+    # plt.xticks(list(range(indexCounter)), list(map(getHeuristicName, heuristics)) + ["VP + EL 2"])
+    # plt.xlabel('heuristiek')
+    # plt.ylabel('tijd (s)')
+    # plt.yscale("log")
+    # ax2 = ax1.twinx()
+    # y_min, y_max = ax1.get_ylim()  # Get the limits from the left y-axis
+    # ax2.set_ylim(y_min, y_max)  # Set the same limits for the right y-axis
+    # # Hide the spines between ax1 and ax2
+    # ax1.spines['right'].set_visible(False)
+    # ax2.spines['left'].set_visible(False)
+    # ax2.yaxis.tick_right()
+    # ax2.set_yscale("log")
+
+    local_file_path = f"figs/randomRatios/heuristics/{vtree}/test_20_{nrOfVars}_{operationStr}_{heuristics+[10]}.png"
+    fullPath = os.path.join("", local_file_path)
+    os.makedirs(os.path.dirname(fullPath), exist_ok=True)
+    plt.savefig(local_file_path)
+    plt.clf() #clear
+
+    
 def __main__():
     heuristics = [99, VO, IVO_LR, IVO_RL, VP_KE, VP_EL]
-    nrOfVars = 24
+    nrOfVars = 28
     vtree = "balanced"
+
     operation = OR
-    heuristicsPlot(heuristics, nrOfVars, vtree, operation) 
-    otherMetricsPlot(heuristics, nrOfVars, vtree, operation)
+    # heuristicsPlot(heuristics, nrOfVars, vtree, operation) 
+    # otherMetricsPlot(heuristics, nrOfVars, vtree, operation)
+    randomRatiosPlot(heuristics, nrOfVars, vtree, operation)
+
     operation = AND
-    heuristicsPlot(heuristics, nrOfVars, vtree, operation) 
-    otherMetricsPlot(heuristics, nrOfVars, vtree, operation)
+    # heuristicsPlot(heuristics, nrOfVars, vtree, operation) 
+    # otherMetricsPlot(heuristics, nrOfVars, vtree, operation)
+    randomRatiosPlot(heuristics, nrOfVars, vtree, operation)
 
 __main__()
 
